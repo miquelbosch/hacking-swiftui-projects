6745fede8d7c291575500e82759e042742748d17
Miquel Bosch <miquel.bosch.cortes@gmail.com>
2021-06-29 17:18:14 +0200

    initial commit - second project

---
+++ WeLength/WeLength/WeLengthApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeLengthApp.swift
+//  WeLength
+//
+//  Created by Miquel Bosch on 29/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeLengthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
