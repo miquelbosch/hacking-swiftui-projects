98383009b8f8e81fd3cd7eedf953b1e8570cbdff
Miquel Bosch <miquel.bosch.cortes@gmail.com>
2021-07-17 19:30:01 +0200

    init commit game flag

---
+++ GameFlag/GameFlag/GameFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameFlagApp.swift
+//  GameFlag
+//
+//  Created by Miquel Bosch on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct GameFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
